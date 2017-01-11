260cc1b80f7c74612b627811062ba9bc0de2a8c1
林博仁 <Buo.Ren.Lin@gmail.com>
2017-01-12 00:15:41 +0800

    The Bash Commons Library.bash - Add header
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- The Bash Commons Library.bash
+++ The Bash Commons Library.bash
@@ -1,1 +1,5 @@
- 
+# This is a GNU Bash library to attempt to make lives easier.
+# https://github.com/Lin-Buo-Ren/The-Bash-Commons-Library
+# 林博仁 © 2016
+# This file is released using WTFPL license
+
