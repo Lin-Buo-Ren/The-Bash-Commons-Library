44317e4f1feae20346bdffc69254aabd097948b6
林博仁 <Buo.Ren.Lin@gmail.com>
2017-01-12 12:42:19 +0800

    實作雛型
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- The Bash Commons Library.bash
+++ The Bash Commons Library.bash
@@ -1,5 +1,48 @@
+#!/usr/bin/env bash
 # This is a GNU Bash library to attempt to make lives easier.
 # https://github.com/Lin-Buo-Ren/The-Bash-Commons-Library
 # 林博仁 © 2016
 # This file is released using WTFPL license
 
+## Meaninful Bash Conditional Expressions ##
+bash_commons_test_if_file_exist(){
+	local file_path="${1}"
+
+	test -a "$file_path"
+# 	Causes a function or sourced script to exit with the return value
+#   specified by N.  If N is omitted, the return status is that of the
+#   last command executed within the function or script.
+	return
+}
+bash_test_if_file_exist(){
+	local file_path="${1}"
+
+	bash_commons_test_if_file_exist "${file_path}"
+	return
+}
+bc_test_if_file_exist(){
+	local file_path="${1}"
+
+	bash_commons_test_if_file_exist "${file_path}"
+	return
+}
+bc_is_file_exist(){
+	local file_path="${1}"
+
+	bash_commons_test_if_file_exist "${file_path}"
+	return
+}
+
+bash_commons_meta_main(){
+	if ! echo "${0}" | grep --quiet "The Bash Commons Library" ; then
+		# Script is source'd instead of run, don't do anything
+		return
+	fi
+	printf "# The Bash Commons Library #\n"
+	printf "Source this file to use the library, or run\n"
+	printf "\n"
+	printf "\t%s --demonstration\n" "${0}"
+	printf "\n"
+	printf "for demonstration.\n"
+}
+bash_commons_meta_main
